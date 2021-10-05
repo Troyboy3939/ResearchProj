9fe213fe06eb4d4b9e6dd1adbc9bed88311a01b4
Troy <troyrooney39@gmail.com>
2021-10-05 14:38:37 +1100

    Started building game - Animations, Weapons

---
+++ Assets/Scripts/SpawnAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAI : MonoBehaviour
+{
+    [SerializeField] GameObject m_AIPrefab;
+    [SerializeField] GameObject m_RedLocation;
+    [SerializeField] GameObject m_BlueLocation;
+    [SerializeField] Material m_BlueMaterial;
+
+    GameObject m_Red;
+    GameObject m_Blue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_AIPrefab != null && m_RedLocation != null && m_BlueLocation != null)
+        {
+            m_Red = Instantiate(m_AIPrefab, m_RedLocation.transform.position, m_RedLocation.transform.rotation);
+            m_Blue = Instantiate(m_AIPrefab, m_BlueLocation.transform.position, m_BlueLocation.transform.rotation);
+
+            m_Blue.GetComponent<MeshRenderer>().material = m_BlueMaterial;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : Weapon
+{
+    [Tooltip("The point at which raycast should be done on this sword, to check if it hit an enemy")]
+    [SerializeField]GameObject m_HitPoint = null;
+
+    //Whether the an enemy has been hit
+    GameObject m_HitEnemy = null;
+
+    //Whether the sword has been swung
+    bool m_SwordSwung = false;
+
+    //Whether the sword is about to be swung
+    bool m_SwingingSword = false;
+
+    //Length of the raycast when swinging sword
+    float m_SwingDistance = 1.0f;
+    
+    //[Tooltip("Collider of where the sword will be swung")]
+    //[SerializeField] BoxCollider m_HitArea = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Type = Type.Sword;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the sword has been swung and there is an enemy
+        if(m_SwordSwung && m_HitEnemy)
+		{
+
+
+            m_SwordSwung = false;
+		}
+
+
+
+
+        m_HitEnemy = null;
+    }
+
+	private void FixedUpdate()
+	{
+		if(m_SwingingSword && m_HitPoint != null)
+		{
+            m_SwordSwung = true;
+            RaycastHit hit = new RaycastHit();
+
+            
+
+            Physics.Raycast(m_HitPoint.transform.position,m_HitPoint.transform.forward * m_SwingDistance,out hit);
+
+            if(hit.transform.tag == "AI")
+			{
+                m_HitEnemy = hit.transform.gameObject;
+                return;
+			}
+
+            m_HitEnemy = null;
+		}
+	}
+
+	public void SetHitEnemy(GameObject enemy)
+	{
+        m_HitEnemy = enemy;
+	}
+
+
+}
